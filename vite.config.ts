3beea7f1a6a9e446e5ea8c4606c127ba6cfed425
vinu <vinu@vinuvarghese.com>
2024-09-13 17:16:36 +0530

    Output to site dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './_site',
+    emptyOutDir: true, // also necessary
+  }
 })
